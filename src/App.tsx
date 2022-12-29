3933b87039857b26fd88b786012e42bf385fc214
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-12-29 22:37:23 +0900

    Add routing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,12 @@
-import useSWR from "swr";
-import {
-  Loading,
-  Student,
-  StudentList,
-  Error,
-  EmptyStudent,
-} from "./components";
-import query from "./query";
-import { StudentType } from "./types";
-
-const fetcher = async (url: string) => (await query.get(url)).data;
+import { Route, Routes } from "react-router-dom";
+import * as P from "./pages";
 
 function App() {
-  const { data, isLoading, error } = useSWR<StudentType[]>(
-    "/students",
-    fetcher,
-    { refreshInterval: 5000 }
-  );
-
-  if (isLoading) return <Loading />;
-
-  if (error) return <Error error={error} />;
-
   return (
-    <>
-      <h1 style={{ textAlign: "center", margin: 0, padding: "2rem" }}>
-        기숙사 입소 학생 정보
-      </h1>
-      {data?.length ? (
-        <StudentList>
-          {data?.sort()?.map((student) => (
-            <Student key={student.id} student={student} />
-          ))}
-        </StudentList>
-      ) : (
-        <EmptyStudent />
-      )}
-    </>
+    <Routes>
+      <Route path="/" element={<P.MainPage />} />
+      <Route path="/signup" element={<P.SignUpPage />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,43 @@
+import useSWR from "swr";
+import {
+  Student,
+  StudentList,
+  EmptyStudent,
+  Loading,
+  Error,
+} from "../components";
+import query from "../query";
+import { StudentType } from "../types";
+
+const fetcher = async (url: string) => (await query.get(url)).data;
+
+const MainPage = () => {
+  const { data, isLoading, error } = useSWR<StudentType[]>(
+    "/students",
+    fetcher,
+    { refreshInterval: 5000 }
+  );
+
+  if (isLoading) return <Loading />;
+
+  if (error) return <Error error={error} />;
+
+  return (
+    <>
+      <h1 style={{ textAlign: "center", margin: 0, padding: "2rem" }}>
+        기숙사 입소 학생 정보
+      </h1>
+      {data?.length ? (
+        <StudentList>
+          {data?.sort()?.map((student) => (
+            <Student key={student.id} student={student} />
+          ))}
+        </StudentList>
+      ) : (
+        <EmptyStudent />
+      )}
+    </>
+  );
+};
+
+export default MainPage;
